9f05f9c1acb9728c23e70512e96230c14ef298ae
Manolo Rajaonah <77987911+ManoloRaj@users.noreply.github.com>
2021-06-27 23:26:32 +0300

    Update Main.java

--- main/Main.java
+++ main/Main.java
@@ -1,6 +1,10 @@
+import basisClass.Medicament;
+import basisClass.DBManage;
 class Main {
 
    public static void main (String[] args){
+      
+      //Medicament med = new Medicament():
    	System.out.println("Hello World");
    }
 }
